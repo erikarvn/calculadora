080b156f0d2f961cd6e27e45a870063d9d37a0e2
irmp <hola>
2014-10-01 22:04:32 -0400

    first commit

--- features/steps/calculadora_steps.rb
+++ features/steps/calculadora_steps.rb
@@ -6,6 +6,7 @@ When(/^ejecuto la calculadora$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+
 Then(/^la salida deberia se "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
